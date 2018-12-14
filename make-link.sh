07e6f92646f273d51baa98ecd5e3e4752bf38338
norihisa.matsumoto <nantsuka2011@gmail.com>
2018-12-14 13:37:49 +0900

    m

--- make-link.sh
+++ make-link.sh
@@ -5,4 +5,5 @@ ln -sf ~/dotfiles/.vrapperrc ~/.vrapperrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.dircolors-solarized ~/.dircolors-solarized
-
+ln -sf ~/dotfiles/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+ln -sf ~/dotfiles/vscode/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings.json"
